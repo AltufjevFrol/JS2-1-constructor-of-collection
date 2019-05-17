7f21f7c7ec1f35037f73f750701368655faaaec9
Frol <artem.mihailovich@gmail.com>
2019-05-17 12:52:24 +0400

    Добавил методы в прототип

--- index.js
+++ index.js
@@ -8,8 +8,61 @@ function Collection() {}
 
 
 // Методы коллекции
-Collection.prototype.values = function () {};
-// другие методы
+/*
+*Метод values возвращает массив элементов коллекци
+*/
+Collection.prototype.values = function () {
+}
+
+/*
+*Метод at возвращает iтое свойство коллекции
+*/
+Collection.prototype.at = function(i) {
+return this[i];
+}
+
+/*
+*Метод append добавляет новые свойства коллекции
+*/
+Collection.prototype.append = function(item) {
+/*
+*если прилетела другая коллекция то разбираем ее и добавляем свойсва
+*/
+if (item instanceof Collection) {
+for (key in item){
+	if (!item.hasOwnProperty(key)) {continue}
+		else {
+			this[getNumProp(this)] = item[key];
+		}
+	}
+}
+/*
+*Если это не наша коллеккция то просто добавляем аргумент
+*в следующее по счету свойство
+*/
+else {
+	this[getNumProp(this)] = item;
+}
+	return this;
+}
+
+/*
+*Метод removeAt удаляет свойства по указанному номеру
+*в случае успеха возвращает true иначе false
+*/
+Collection.prototype.removeAt = function(i) {
+if (this.hasOwnProperty(i)){
+	delete this[i];
+	return true
+}
+else return false;
+}
+
+//вспомогательная функия возвращает номер следуюещего свойства
+function getNumProp (collection){
+	return Object.keys(collection).length + 1;
+}
+
 
 
 /**
