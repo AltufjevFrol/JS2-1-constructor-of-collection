c9326eb2f6f43fc4cff23aaa8c8ab5fcdef66912
Frol <artem.mihailovich@gmail.com>
2019-05-18 23:35:56 +0400

    Решение задачи самым простым путем. Посколько коллекция ведет себя как массив, то все экземпляры Collection имеют свойство list которое массив и содержит. А дальше методы работают с этим свойством как с массивом.

--- index.js
+++ index.js
@@ -1,18 +1,69 @@
 module.exports = Collection;
+/*
+*Конструктор коллекции
+*/
+function Collection(){
+this.list = [];
+}
 
-/**
- * Конструктор коллекции
- * @constructor
- */
-function Collection() {}
 
+/*
+* Создание коллекции из массива значений
+*/
+Collection.from = function (arrItems) {
+var obj = new Collection();
+obj.list = obj.list.concat(arrItems)
+return obj;
+}
 
 // Методы коллекции
-Collection.prototype.values = function () {};
-// другие методы
 
+/*
+*Метод values возвращает массив элементов коллекци
+*/
+Collection.prototype.values = function () {
+return this.list;
+}
+/*
+*Метод at возвращает iтое свойство коллекции
+*/
+Collection.prototype.at = function(i) {
+if (i<=0 || !Math.floor(i)) return null;//целый, положительный, не ноль
+else if (this.list[i-1] === undefined) return null;// элемент отсутствует
+else return this.list[i-1];
+}
 
-/**
- * Создание коллекции из массива значений
- */
-Collection.from = function () {};
+/*
+*Метод append добавляет новые свойства коллекции
+*/
+Collection.prototype.append = function(item) {
+/*
+*если прилетела другая коллекция то разбираем ее и добавляем свойсва
+*/
+if (item instanceof Collection)
+	this.list = this.list.concat(item.list);
+	/*
+*Если это не наша коллеккция то просто добавляем аргумент
+*в следующее по счету свойство
+*/
+else 
+	this.list = this.list.concat(item);
+}
+
+/*
+*Метод removeAt удаляет свойства по указанному номеру
+*в случае успеха возвращает true иначе false
+*/
+Collection.prototype.removeAt = function(i) {
+if (i<=0 || !Math.floor(i)) return false;//целый, положительный, не ноль
+if (--i>=this.list.length) return false;// хотим отобрать больше чем есть
+this.list.splice(--i,1);
+return true;
+}
+
+/*
+*Метод count не описан но присутствует в проверках
+*/
+Collection.prototype.count = function(){
+return this.list.length;// вот так вот 
+}
